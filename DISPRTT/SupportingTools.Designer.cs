83819be84a79dd80c1f19ac514132cf90008e5d1
Katyashka <ryapolova.katya@yandex.ru>
2019-03-20 13:50:30 +0500

    SomeTest

--- DISPRTT/SupportingTools.Designer.cs
+++ DISPRTT/SupportingTools.Designer.cs
@@ -101,6 +101,7 @@
             this.удалитьToolStripMenuItem.Name = "удалитьToolStripMenuItem";
             this.удалитьToolStripMenuItem.Size = new System.Drawing.Size(88, 29);
             this.удалитьToolStripMenuItem.Text = "Удалить";
+            this.удалитьToolStripMenuItem.Click += new System.EventHandler(this.удалитьToolStripMenuItem_Click);
             // 
             // изменитьToolStripMenuItem
             // 
