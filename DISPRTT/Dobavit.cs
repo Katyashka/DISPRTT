fb6c5e8c8cb94e9a7c709b9983985068313ba85d
DaryaSar <daryasarlova@gmail.com>
2019-04-10 21:43:35 +0500

    добавление параметров в форму ДОБАВИТЬ

--- DISPRTT/Dobavit.cs
+++ DISPRTT/Dobavit.cs
@@ -17,13 +17,51 @@ namespace DISPRTT
             prd.dataAdapter.InsertCommand = new SqlCommand("AddPredmet");
             prd.dataAdapter.InsertCommand.Connection = prd.dataAdapter.SelectCommand.Connection;
             prd.dataAdapter.InsertCommand.CommandType = CommandType.StoredProcedure;
-            SqlParameter dob = new SqlParameter
+            prd.dataAdapter.InsertCommand = new SqlCommand("GetNastroyky");
+            SqlParameter fkParameter = new SqlParameter
             {
-                ParameterName = "@nazvanie",
+                ParameterName = "@fk_vt",
+                Value = comboBox2.Text 
+            };
+            prd.dataAdapter.InsertCommand.Parameters.Add(fkParameter);
+            SqlParameter kodParameter = new SqlParameter
+            {
+                ParameterName = "@kod",
                 Value = textBox1.Text
             };
-            prd.dataAdapter.InsertCommand.Parameters.Add(dob);
-            prd.dataAdapter.InsertCommand.ExecuteNonQuery();
+            prd.dataAdapter.InsertCommand.Parameters.Add(kodParameter);
+            SqlParameter nazvanieParameter = new SqlParameter
+            {
+                ParameterName = "@nazvanie",
+                Value = textBox2.Text
+            };
+            prd.dataAdapter.InsertCommand.Parameters.Add(nazvanieParameter);
+            SqlParameter kodprintParameter = new SqlParameter
+            {
+                ParameterName = "@kodprint",
+                Value = textBox3.Text
+            };
+            prd.dataAdapter.InsertCommand.Parameters.Add(kodprintParameter);
+            SqlParameter nameParameter = new SqlParameter
+            {
+                ParameterName = "@name",
+                Value = textBox4.Text
+            };
+            prd.dataAdapter.InsertCommand.Parameters.Add(nameParameter);
+            SqlParameter minballParameter = new SqlParameter
+            {
+                ParameterName = "@minball",
+                Value = textBox5.Text
+            };
+            prd.dataAdapter.InsertCommand.Parameters.Add(minballParameter);
+            SqlParameter projectfileParameter = new SqlParameter
+            {
+                ParameterName = "@projectfile",
+                Value = comboBox1.Text
+            };
+            prd.dataAdapter.InsertCommand.Parameters.Add(projectfileParameter);
+            prd.dataAdapter.InsertCommand.ExecuteScalar();
+
         }
 
 
